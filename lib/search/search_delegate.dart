6af7be3b2633be58b49e26adda70d18f14105711
Josep Capó <jcapo@victu.es>
2021-01-21 10:53:26 +0100

    implement correcty the buildresults function and remove the non necessary code

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -5,24 +5,7 @@ import 'package:flutter_themoviedb/providers/movies_provider.dart';
 class DataSearch extends SearchDelegate {
   String selection = "";
   final _moviesProvider = MoviesProvider();
-
-  final movies = [
-    "Spiderman",
-    "Aquaman",
-    "Batman",
-    "Shazam!",
-    "Ironman 1",
-    "Ironman 2",
-    "Ironman 3",
-    "Ironman 4",
-    "Ironman 5",
-    "Capitan America",
-  ];
-
-  final recentMovies = [
-    "Spiderman",
-    "Capitan America",
-  ];
+  List<MovieModel> _moviesList;
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -54,13 +37,24 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // It builds the results we will show
-    return Center(
-      child: Container(
-        height: 100,
-        width: 100,
-        color: Colors.blueAccent,
-        child: Text(selection),
-      ),
+    return ListView(
+      children: _moviesList.map((m) {
+        return ListTile(
+          leading: FadeInImage(
+            image: NetworkImage(m.getPosterImg()),
+            placeholder: AssetImage("assets/img/no-image.jpg"),
+            width: 50,
+            fit: BoxFit.contain,
+          ),
+          title: Text(m.title),
+          subtitle: Text(m.originalTitle),
+          onTap: () {
+            close(context, null);
+            m.uniqueId = "";
+            Navigator.pushNamed(context, "detail", arguments: m);
+          },
+        );
+      }).toList(),
     );
   }
 
@@ -76,10 +70,10 @@ class DataSearch extends SearchDelegate {
       future: _moviesProvider.searchMovie(query),
       builder: (_, AsyncSnapshot<List<MovieModel>> snapshot) {
         if (snapshot.hasData) {
-          final movies = snapshot.data;
+          _moviesList = snapshot.data;
 
           return ListView(
-            children: movies.map((m) {
+            children: _moviesList.map((m) {
               return ListTile(
                 leading: FadeInImage(
                   image: NetworkImage(m.getPosterImg()),
